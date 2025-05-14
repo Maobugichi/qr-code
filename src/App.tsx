f24eba3f0346b0830e7b8cebe483c80ecb2c3105
Maobugichi <immansamuel846@gmail.com>
2025-05-14 12:39:42 -0700

    add border solid

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import QRCode from "react-qr-code";
 function App() {
   return (
     <div className="w-full grid place-items-center h-[80vh]">
-      <div className="border-gray-400 border  w-[60%] md:w-[18%] h-[260px] md:h-[260px]  rounded-lg flex flex-col justify-center gap-1 items-center">
+      <div className="border-gray-400 border border-solid w-[60%] md:w-[18%] h-[260px] md:h-[260px]  rounded-lg flex flex-col justify-center gap-1 items-center">
        
         <QRCode className="w-[90%]" value='https://maobugichi.github.io/wink-and-flirt'/>
         <h2 className="text-lg">connect with us</h2>

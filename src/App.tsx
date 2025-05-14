5f80c6da2bd512d94e9755e4622c355a6d86fb97
Maobugichi <immansamuel846@gmail.com>
2025-05-14 12:37:50 -0700

    initial commit2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import QRCode from "react-qr-code";
 function App() {
   return (
-    <>
-      <div className="border-gray-400 border  w-[15%] h-[200px] mt-10 ml-10 rounded-lg flex flex-col justify-center gap-1 items-center">
+    <div className="w-full grid place-items-center h-[80vh]">
+      <div className="border-gray-400 border  w-[60%] md:w-[18%] h-[260px] md:h-[260px]  rounded-lg flex flex-col justify-center gap-1 items-center">
        
-        <QRCode size={150} value='https://maobugichi.github.io/wink-and-flirt'/>
-        <h2>connect with us</h2>
+        <QRCode className="w-[90%]" value='https://maobugichi.github.io/wink-and-flirt'/>
+        <h2 className="text-lg">connect with us</h2>
       </div>
-    </>
+    </div>
   )
 }
 
